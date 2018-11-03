8d18a625829ae19709c2d292a37ada3301a52144
Ihor Ahnianikov <ahnianikov@gmail.com>
2018-11-03 15:18:06 +0000

    Fix tests

---
+++ tests/test-favorite.php
@@ -0,0 +1,13 @@
+<?php
+namespace HivePress\Favorites;
+
+/**
+ * Tests favorites.
+ *
+ * @class Favorite_Test
+ */
+class Favorite_Test extends \WP_UnitTestCase {
+	public function test_update() {
+		$this->assertTrue( true );
+	}
+}
