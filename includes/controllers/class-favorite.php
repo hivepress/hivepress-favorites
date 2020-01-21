525db7d390bf19b24d57552368d732dec591b43b
Ihor Ahnianikov <ahnianikov@gmail.com>
2020-01-21 22:59:48 +0200

    Fix compatibility

--- includes/controllers/class-favorite.php
+++ includes/controllers/class-favorite.php
@@ -67,7 +67,7 @@ final class Favorite extends Controller {
 		}
 
 		// Get listing.
-		$listing = Models\Listing::query()->get_by_id( $request->get_param( 'listing_id' ) );
+		$listing = Models\Listing::query()->get_by_id( $request->get_param( 'listing' ) );
 
 		if ( empty( $listing ) || $listing->get_status() !== 'publish' ) {
 			return hp\rest_error( 404 );
