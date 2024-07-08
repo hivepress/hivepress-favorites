1ebe4c04bdbb196f3509a169d3bbfcf4df4be925
kuznev <kuznietevhen@gmail.com>
2024-07-08 12:32:05 +0300

    Add favorite count

--- includes/controllers/class-favorite.php
+++ includes/controllers/class-favorite.php
@@ -82,10 +82,19 @@ final class Favorite extends Controller {
 			]
 		)->get();
 
+		// Get favorite count.
+		$favorite_count = $listing->get_favorite_count();
+
 		if ( $favorites->count() ) {
 
 			// Delete favorites.
 			$favorites->delete();
+
+			if ( get_option( 'hp_listing_count_favorite' ) && $favorite_count ) {
+
+				// Decrease favorite count.
+				$listing->set_favorite_count( $favorite_count - 1 )->save_favorite_count();
+			}
 		} else {
 
 			// Add favorite.
@@ -98,6 +107,12 @@ final class Favorite extends Controller {
 
 			if ( ! $favorite->save() ) {
 				return hp\rest_error( 400, $favorite->_get_errors() );
+			}
+
+			if ( get_option( 'hp_listing_count_favorite' ) ) {
+
+				// Increase favorite count.
+				$listing->set_favorite_count( $favorite_count + 1 )->save_favorite_count();
 			}
 		}
 
